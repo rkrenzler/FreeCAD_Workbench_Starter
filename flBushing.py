0f890207b865710b657b75cb5b82722a4804f857
Ruslan <20125922+rkrenzler@users.noreply.github.com>
2018-04-29 15:46:10 +0200

    update flamingo version of the bushing

--- flBushing.py
+++ flBushing.py
@@ -6,113 +6,114 @@
 import FreeCAD, Part
 import math
 from pipeFeatures import pypeType #the parent class
-
+import bushing as bushingMod
 
 class Bushing(pypeType):
-	def __init__(self, obj, PSize="", L=30, N=20, POD=40, POD1=20, PID1=10):
+	def __init__(self, obj, PSize="", dims=bushingMod.Dimensions()):
 		"""Create a bushing"""
 	
 		# Run parent __init__ and define common attributes
 		super(Bushing, self).__init__(obj)
 		obj.PType="OSE_Bushing"
-		obj.PRating=""
+		obj.PRating="BushingFittingFromAnyCatalog"
 		obj.PSize=PSize # What is it for?
 		# Define specific attributes and set their values.
-		obj.addProperty("App::PropertyLength","L","Bushing","Bushing length").L=L
-		obj.addProperty("App::PropertyLength","N","Bushing","N").N=N
-		obj.addProperty("App::PropertyLength","POD","Bushing","Large pipe outer diameter.").POD=POD
-		obj.addProperty("App::PropertyLength","POD1","Bushing","Small pipe outer diameter.").POD1=POD1
-		obj.addProperty("App::PropertyLength","PID1","Bushing","Small pipe inner diameter.").PID1=PID1
+		obj.addProperty("App::PropertyLength","L","Bushing","Bushing length").L=dims.L
+		obj.addProperty("App::PropertyLength","N","Bushing","N").N=dims.N
+		obj.addProperty("App::PropertyLength","POD","Bushing","Large pipe outer diameter.").POD=dims.POD
+		obj.addProperty("App::PropertyLength","POD1","Bushing","Small pipe outer diameter.").POD1=dims.POD1
+		obj.addProperty("App::PropertyLength","PThk1","Bushing","Small pipe thickness.").PThk1=dims.PThk1
 		obj.addProperty("App::PropertyVectorList","Ports","Bushing","Ports relative positions.").Ports = self.getPorts(obj)
+		obj.addProperty("App::PropertyString","PartNumber","Bushing","Part number").PartNumber=""				
 		# Make Ports read only.
 		obj.setEditorMode("Ports", 1)
 
 	def onChanged(self, obj, prop):
 		# Attributes changed, adjust the rest.
-		if prop in ["N"]:
+		if prop in [ "L", "N"]:
 			# This function is called within __init__ too. Thus we need to wait untill 
 			# we have all the required attributes.
 			if "Ports" in obj.PropertiesList:
 				obj.Ports = self.getPorts(obj)
-	@staticmethod
-	def createOctaThing(obj):
+				
+	@classmethod
+	def extractDimensions(cls, obj):
+		dims = bushingMod.Dimensions()
+		dims.L = obj.L
+		dims.N = obj.N
+		dims.POD = obj.POD
+		dims.POD1 = obj.POD1
+		dims.PThk1 = obj.PThk1
+		return dims
+		
+	@classmethod
+	def createOctaThing(cls, obj):
 		""" Create Octagonal thing at the end of the bushing. I do not know its name."""
-		L = obj.L
-		N = obj.N
-		POD = obj.POD
-		# I do not know how to calculate X, there fore I just
-		# take a half of (L-N)
-		X1 = (L-N)/2
-		# I also do not know what is the size of the thing.
-		# I take 1.2 of the inner diameter of the larger pipe
-		X2 = POD*1.1
+		dims = cls.extractDimensions(obj)
+		aux = dims.auxiliararyPoints()
+		X1 = dims.ThingThicknessA1()
+		X2 = dims.ThingLengthA2()
 		# Move the box into the center of the X,Y plane.
-		center_pos = FreeCAD.Vector(-X2/2, -X2/2, L-X1)
+		center_pos = FreeCAD.Vector(-X2/2, -X2/2, 0)+aux["p4"]
 		box1 = Part.makeBox(X2,X2,X1, center_pos)
 		# rotate a box by 45° around the z.axis.
 		box2 = Part.makeBox(X2,X2,X1, center_pos)
 		box2.rotate(FreeCAD.Vector(0,0,0),FreeCAD.Vector(0,0,1),45)
 		return box1.common(box2)
 		
-	@staticmethod
-	def createOuterPart(obj):
-		L = obj.L
-		POD = obj.POD
-		outer_cylinder = Part.makeCylinder(POD/2, L)
+	@classmethod
+	def createOuterPart(cls, obj):
+		dims = cls.extractDimensions(obj)
+		aux = dims.auxiliararyPoints()		
+		outer_cylinder = Part.makeCylinder(dims.POD/2, dims.L, aux["p1"])
 		thing = Bushing.createOctaThing(obj)
 		return outer_cylinder.fuse(thing)
 		
-	@staticmethod
-	def createInnerPart(obj):
-		L = obj.L
-		N = obj.N
-		POD = obj.POD
-		PID1 = obj.PID1
-		POD1 = obj.POD1
+	@classmethod
+	def createInnerPart(cls, obj):
+		dims = cls.extractDimensions(obj)
+		aux = dims.auxiliararyPoints()
 		
 		# Remove inner part of the sockets.
-		inner_cylinder = Part.makeCylinder(PID1/2, L)
-		inner_socket = Part.makeCylinder(POD1/2, L - N, FreeCAD.Vector(0,0,N))
+		inner_cylinder = Part.makeCylinder(dims.PID1()/2, dims.L, aux["p1"])
+		inner_socket = Part.makeCylinder(dims.POD1/2, dims.L - dims.N, aux["p3"])
 
 		# Make a cone for a larger socket. There are no dimensions for this con. Therefore 
 		# use simbolically a Radius such that the wall at the lower end is twice as thick
 		# as in the upper end of socket.
-		W2 = (POD-PID1)/2
-		hcone = N/2 # I do not know what the hight of the cone should be.
-				# I just take a half. 
+		r1 = dims.POD/2 - dims.ThicknessA3()
+		r2 = dims.PID1()/2
+		hcone = dims.ConeLengthA4()
+		socket_cone = Part.makeCone(r1, r2, hcone, aux["p1"])
 
-		socket_cone = Part.makeCone(PID1/2 + W2/2, PID1/2, hcone)
-		
 		return inner_cylinder.fuse([inner_socket, socket_cone])
 		
-	@staticmethod	
-	def createShape(obj):
-		outer = Bushing.createOuterPart(obj)
-		inner = Bushing.createInnerPart(obj)
+	@classmethod	
+	def createShape(cls, obj):
+		outer = cls.createOuterPart(obj)
+		inner = cls.createInnerPart(obj)
 		return outer.cut(inner)
 
 	def execute(self, obj):
 		# Create the shape of the bushing.
-		shape = Bushing.createShape(obj)
+		shape = self.createShape(obj)
 		obj.Shape = shape
 		# Recalculate ports.
 		obj.Ports = self.getPorts(obj)
 		
 	def getPorts(self, obj):
 		""" Calculate coordinates of the ports. """
-		port_bottom = FreeCAD.Vector(0,0, 0)
-		port_top = FreeCAD.Vector(0,0, obj.N)
-		return [port_bottom, port_top]
+		dims = self.extractDimensions(obj)
+		aux = dims.auxiliararyPoints()
+		# For the bottom port use p3 too. Because there is no a1 dimension in my specification.
+		return[aux["p3"], aux["p3"]]
 
 class BushingBuilder:
 	""" Create a bushing using flamingo. """
 	def __init__(self, document):
+		self.dims = bushingMod.Dimensions()
 		self.pos = FreeCAD.Vector(0,0,0) # Define default initial position.
-		self.L = 30
-		self.N = 20
-		self.POD = 40
-		self.POD1 = 20
-		self.PID1 = 10
+		self.document = document
 		
 	def create(self, obj):
 		"""Create a bushing.
@@ -120,9 +121,10 @@ class BushingBuilder:
 		Before call it, call
 		feature = self.document.addObject("Part::FeaturePython","OSE-Bushing")
 		"""			
-		bushing = Bushing(obj, PSize="", L=self.L, N=self.N, POD=self.POD, POD1=self.POD1, PID1=self.PID1)
+		bushing = Bushing(obj, PSize="", dims=self.dims)
 		obj.ViewObject.Proxy = 0
 		obj.Placement.Base = self.pos
+
 		return bushing
 
 # Create a test bushing.
@@ -133,4 +135,4 @@ def Test():
 	builder.create(feature)
 	document.recompute()
 	
-Test()
+#Test()
