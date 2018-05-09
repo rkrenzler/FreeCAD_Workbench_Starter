8a2cb2b78dd61d68350a7caa58e5d5601f42430e
Ruslan <20125922+rkrenzler@users.noreply.github.com>
2018-05-09 22:08:53 +0200

    use createPartGui module for pipeGui too.

--- pipeGui.py
+++ pipeGui.py
@@ -6,88 +6,30 @@
 import math
 import csv
 import os.path
+
+import pipe
 import createPartGui
 
 from PySide import QtCore, QtGui
 import FreeCAD
 
-import OSEBase
-from pipe import *
-from piping import *
-import pipingGui
-
-
-class MainDialog(QtGui.QDialog):
-	QSETTINGS_APPLICATION = "OSE piping workbench"
-	QSETTINGS_NAME = "pipe user input"
-
+class MainDialog(createPartGui.BaseDialog):
 	def __init__(self, document, table):
-		super(MainDialog, self).__init__()
-		self.document = document
-		self.table = table
-		self.selectionMode = False
-		self.selectedPart = None
-		self.initUi()
+		params = createPartGui.DialogParams()
+		params.document = document
+		params.table = table
+		params.dialogTitle = "Create Pipe"
+		params.fittingType = "Pipe"
+		params.dimensionsPixmap = "pipe-dimensions.png"
+		params.explanationText = "<html><head/><body><p>To construct a part, only these dimensions are used: OD, Thk and the pipe height (length). Flamingo also uses Schedule and DN if they are present in the table. All other dimensions are used for inromation.</p></body></html>"
+		params.keyColumnName = "PartNumber"
+		super(MainDialog, self).__init__(params)
 
-	def initUi(self): 
-		Dialog = self # Added 
-		self.result = -1 
-		self.setupUi(self)
-		# Fill table with dimensions. 
-		self.initTable()
-
-		# Restore previous user input. Ignore exceptions to prevent this part
-		# part of the code to prevent GUI from starting, once settings are broken.
-		try:
-			self.restoreInput()
-		except Exception as e:
-			print ("Could not restore old user input!")
-			print(e)
-
-		if HasFlamingoSupport():
-			self.radioButtonFlamingo.setEnabled(True)			
-		else:
-			self.radioButtonFlamingo.setEnabled(False)
-			
-		self.show()
-
-# The following lines are from QtDesigner .ui-file processed by pyside-uic
-# pyside-uic --indent=0 create-pipe.ui -o tmp.py
-# You need to adjust image paths to
-# os.path.join(OSEBase.IMAGE_PATH, "pipe-dimensions.png")
-	def setupUi(self, Dialog):
-		Dialog.setObjectName("Dialog")
-		Dialog.resize(836, 633)
-		self.verticalLayout = QtGui.QVBoxLayout(Dialog)
-		self.verticalLayout.setObjectName("verticalLayout")
-		self.horizontalWidget = QtGui.QWidget(Dialog)
-		self.horizontalWidget.setMinimumSize(QtCore.QSize(0, 55))
-		self.horizontalWidget.setLayoutDirection(QtCore.Qt.LeftToRight)
-		self.horizontalWidget.setObjectName("horizontalWidget")
-		self.groupBox = QtGui.QGroupBox(self.horizontalWidget)
-		self.groupBox.setGeometry(QtCore.QRect(10, 0, 263, 58))
-		self.groupBox.setObjectName("groupBox")
-		self.horizontalLayout = QtGui.QHBoxLayout(self.groupBox)
-		self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-		self.horizontalLayout.setObjectName("horizontalLayout")
-		self.radioButtonSolid = QtGui.QRadioButton(self.groupBox)
-		self.radioButtonSolid.setEnabled(True)
-		self.radioButtonSolid.setChecked(True)
-		self.radioButtonSolid.setObjectName("radioButtonSolid")
-		self.horizontalLayout.addWidget(self.radioButtonSolid)
-		self.radioButtonFlamingo = QtGui.QRadioButton(self.groupBox)
-		self.radioButtonFlamingo.setEnabled(False)
-		self.radioButtonFlamingo.setChecked(False)
-		self.radioButtonFlamingo.setObjectName("radioButtonFlamingo")
-		self.horizontalLayout.addWidget(self.radioButtonFlamingo)
-		self.radioButtonParts = QtGui.QRadioButton(self.groupBox)
-		self.radioButtonParts.setObjectName("radioButtonParts")
-		self.horizontalLayout.addWidget(self.radioButtonParts)
-		self.verticalLayout.addWidget(self.horizontalWidget)
-		self.widgetHeight = QtGui.QWidget(Dialog)
-		self.widgetHeight.setMinimumSize(QtCore.QSize(0, 27))
-		self.widgetHeight.setObjectName("widgetHeight")
-		self.labelHeight = QtGui.QLabel(self.widgetHeight)
+	def createLengthWidget(self, Dialog):
+		self.widgetLengthInput = QtGui.QWidget(Dialog)
+		self.widgetLengthInput.setMinimumSize(QtCore.QSize(0, 27))
+		self.widgetLengthInput.setObjectName("widgetLengthInput")
+		self.labelHeight = QtGui.QLabel(self.widgetLengthInput)
 		self.labelHeight.setGeometry(QtCore.QRect(0, 0, 121, 27))
 		sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
 		sizePolicy.setHorizontalStretch(0)
@@ -97,187 +39,28 @@ class MainDialog(QtGui.QDialog):
 		self.labelHeight.setMinimumSize(QtCore.QSize(0, 27))
 		self.labelHeight.setMaximumSize(QtCore.QSize(200, 16777215))
 		self.labelHeight.setObjectName("labelHeight")
-		self.lineEditLength = QtGui.QLineEdit(self.widgetHeight)
+		self.lineEditLength = QtGui.QLineEdit(self.widgetLengthInput)
 		self.lineEditLength.setGeometry(QtCore.QRect(120, 0, 91, 27))
 		self.lineEditLength.setObjectName("lineEditLength")
-		self.verticalLayout.addWidget(self.widgetHeight)
-		self.horizontalLayoutWithTable = QtGui.QHBoxLayout()
-		self.horizontalLayoutWithTable.setContentsMargins(-1, -1, -1, 19)
-		self.horizontalLayoutWithTable.setObjectName("horizontalLayoutWithTable")
-		self.labelImage = QtGui.QLabel(Dialog)
-		self.labelImage.setText("")
-		self.labelImage.setPixmap(os.path.join(OSEBase.IMAGE_PATH, "pipe-dimensions.png"))
-		self.labelImage.setAlignment(QtCore.Qt.AlignCenter)
-		self.labelImage.setObjectName("labelImage")
-		self.horizontalLayoutWithTable.addWidget(self.labelImage)
-		self.tableViewParts = QtGui.QTableView(Dialog)
-		self.tableViewParts.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)
-		self.tableViewParts.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
-		self.tableViewParts.setObjectName("tableViewParts")
-		self.horizontalLayoutWithTable.addWidget(self.tableViewParts)
-		self.verticalLayout.addLayout(self.horizontalLayoutWithTable)
-		self.labelExplanation = QtGui.QLabel(Dialog)
-		self.labelExplanation.setTextFormat(QtCore.Qt.AutoText)
-		self.labelExplanation.setWordWrap(True)
-		self.labelExplanation.setObjectName("labelExplanation")
-		self.verticalLayout.addWidget(self.labelExplanation)
-		self.buttonBox = QtGui.QDialogButtonBox(Dialog)
-		self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-		self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
-		self.buttonBox.setObjectName("buttonBox")
-		self.verticalLayout.addWidget(self.buttonBox)
-
-		self.retranslateUi(Dialog)
-		QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), Dialog.accept)
-		QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), Dialog.reject)
-		QtCore.QMetaObject.connectSlotsByName(Dialog)
-		Dialog.setTabOrder(self.radioButtonSolid, self.radioButtonFlamingo)
-		Dialog.setTabOrder(self.radioButtonFlamingo, self.radioButtonParts)
-		Dialog.setTabOrder(self.radioButtonParts, self.lineEditLength)
-		Dialog.setTabOrder(self.lineEditLength, self.buttonBox)
-
-	def retranslateUi(self, Dialog):
-		Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Create pipe", None, QtGui.QApplication.UnicodeUTF8))
-		self.groupBox.setTitle(QtGui.QApplication.translate("Dialog", "Output type:", None, QtGui.QApplication.UnicodeUTF8))
-		self.radioButtonSolid.setText(QtGui.QApplication.translate("Dialog", "Solid", None, QtGui.QApplication.UnicodeUTF8))
-		self.radioButtonFlamingo.setText(QtGui.QApplication.translate("Dialog", "Flamingo", None, QtGui.QApplication.UnicodeUTF8))
-		self.radioButtonParts.setText(QtGui.QApplication.translate("Dialog", "Parts", None, QtGui.QApplication.UnicodeUTF8))
+		# Add text.
 		self.labelHeight.setText(QtGui.QApplication.translate("Dialog", "Height (Length):", None, QtGui.QApplication.UnicodeUTF8))
-		self.lineEditLength.setText(QtGui.QApplication.translate("Dialog", "1 m", None, QtGui.QApplication.UnicodeUTF8))
-		self.labelExplanation.setText(QtGui.QApplication.translate("Dialog", "<html><head/><body><p>To construct a part, only these dimensions are used: OD, Thk and the pipe height (length). Flamingo also uses Schedule and DN if they are present in the table. All other dimensions are used for inromation.</p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
-	def initTable(self):
-		# Read table data from CSV
-		self.model = pipingGui.PartTableModel(self.table.headers, self.table.data)
-		self.model.keyColumnName = "PartNumber"
-		self.tableViewParts.setModel(self.model)
+		self.lineEditLength.setText(QtGui.QApplication.translate("Dialog", "1 m", None, QtGui.QApplication.UnicodeUTF8))		
+		return self.widgetLengthInput
 		
-	def getSelectedPartName(self):
-		sel = self.tableViewParts.selectionModel()
-		if sel.isSelected:
-			if len(sel.selectedRows())> 0:
-				rowIndex = sel.selectedRows()[0].row()
-				return self.model.getPartKey(rowIndex)
-		return None
-
-	def selectPartByName(self, partName):
-		"""Select first row with a part with a name partName."""
-		if partName is not None:
-			row_i = self.model.getPartRowIndex(partName)
-			if row_i >= 0:
-				self.tableViewParts.selectRow(row_i)
-
-	def accept(self):
-		if self.selectionMode:
-			return self.acceptSelectionMode()
-		else:
-			self.acceptCreationMode()
-			
-	def acceptCreationMode(self):
-		"""User clicked OK"""
-		# If there is no active document, show a warning message and exit dialog.
-		if self.document is None:
-			text = "I have not found any active document were I can create a corner fitting.\n"\
-				"Use menu File->New to create a new document first, "\
-				"then try to create the corner fitting again."
-			msgBox = QtGui.QMessageBox(QtGui.QMessageBox.Warning, "Creating of the corner fitting failed.", text)
-			msgBox.exec_()
-			super(MainDialog, self).accept()
-			return
-
-		# Get suitable row from the the table.
-		length = tu(self.lineEditLength.text())
-		if length == "":
-			msgBox = QtGui.QMessageBox()
-			msgBox.setText("Set length.")
-			msgBox.exec_()
-			return
-
-		partName = self.getSelectedPartName()
-
-		if partName is not None:
-			outputType = self.getOutputType()
-			pipe = PipeFromTable(self.document, self.table)
-			part = pipe.create(partName, length, outputType)
-			if part is not None:
-				self.document.recompute()
-				# Save user input for the next dialog call.
-				self.saveInput()
-				# Call parent class.
-				super(MainDialog, self).accept()
-		else:
-			msgBox = QtGui.QMessageBox()
-			msgBox.setText("Select part")
-			msgBox.exec_()
-
-	def acceptSelectionMode(self):
-		self.selectedPart = self.getSelectedPartName()
-
-		if self.selectedPart is None:
-			msgBox = QtGui.QMessageBox()
-			msgBox.setText("Select part")
-			msgBox.exec_()
-		else:
-			super(MainDialog, self).accept()
-			
-	def saveInput(self):
-		"""Store user input for the next run."""
-		settings = QtCore.QSettings(MainDialog.QSETTINGS_APPLICATION, MainDialog.QSETTINGS_NAME)
-
-		if self.radioButtonFlamingo.isChecked():
-			settings.setValue("radioButtonsOutputType", OUTPUT_TYPE_FLAMINGO)
-		elif self.radioButtonParts.isChecked():
-			settings.setValue("radioButtonsOutputType", OUTPUT_TYPE_PARTS)
-		else : # Default is solid.
-			settings.setValue("radioButtonsOutputType", OUTPUT_TYPE_SOLID)
+	# Add customized UI elements after the type. That is the length ui
+	def setupUi(self, Dialog):
+		super(MainDialog, self).setupUi(Dialog)
+		# Append UI for pipe length input after the output type block.
+		after_index = self.verticalLayout.indexOf(self.outputTypeWidget) + 1
+		widget = self.createLengthWidget(Dialog)
+		self.verticalLayout.insertWidget(after_index, widget)
 		
-		settings.setValue("LastSelectedPartNumber", self.getSelectedPartName())
-		settings.setValue("lineEditLength", self.lineEditLength.text())
-		
-		settings.sync()
+	
+	def createNewPart(self, document, table, partName, outputType):
+		length = FreeCAD.Units.parseQuantity(self.lineEditLength.text())
+		builder = pipe.PipeFromTable(self.params.document, self.params.table)
+		return builder.create(partName, length, outputType)
 
-	def restoreInput(self):
-		settings = QtCore.QSettings(MainDialog.QSETTINGS_APPLICATION, MainDialog.QSETTINGS_NAME)
-
-		output = int(settings.value("radioButtonsOutputType", OUTPUT_TYPE_SOLID))
-		if output == OUTPUT_TYPE_FLAMINGO and HasFlamingoSupport():
-			self.radioButtonFlamingo.setChecked(True)			
-		elif  output == OUTPUT_TYPE_PARTS:
-			self.radioButtonParts.setChecked(True)
-		else: # Default is solid. output == OUTPUT_TYPE_SOLID
-			self.radioButtonSolid.setChecked(True)
-
-		self.selectPartByName(settings.value("LastSelectedPartNumber"))
-		text = settings.value("lineEditLength")
-		if text is not None:
-			self.lineEditLength.setText(text)
-
-	def showForSelection(self, partName=None):
-		""" Show pipe dialog, to select pipe and not to create it.
-		:param partName: name of the part to be selected. Use None if you do not want to select
-		anything.
-		"""
-		# If required select
-		self.selectionMode = True
-		self.setWindowTitle(QtGui.QApplication.translate("Dialog", "Select pipe", None, QtGui.QApplication.UnicodeUTF8))
-		self.selectedPart = None
-		if partName is not None:
-			self.selectPartByName(partName)
-		self.exec_()
-		return self.selectedPart
-		
-	def showForCreation(self):
-		self.selectionMode = False
-		Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Create pipe", None, QtGui.QApplication.UnicodeUTF8))
-		self.exec_()
-		
-	def getOutputType(self):
-		if self.radioButtonFlamingo.isChecked():
-		        FreeCAD.Console.PrintMessage("\n Output flamingo\n") 
-			return OUTPUT_TYPE_FLAMINGO
-		elif self.radioButtonParts.isChecked():
-			return OUTPUT_TYPE_PARTS
-		else: # Default is solid.
-			return OUTPUT_TYPE_SOLID
 
 def GuiCheckTable():
-	return createPartGui.GuiCheckTable2(CSV_TABLE_PATH, DIMENSIONS_USED)
+	return createPartGui.GuiCheckTable2(pipe.CSV_TABLE_PATH, pipe.DIMENSIONS_USED)
