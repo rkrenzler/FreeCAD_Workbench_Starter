6f1a7a1069ed189d9ae19f9137cec9d1ccc005ce
Ruslan <20125922+rkrenzler@users.noreply.github.com>
2018-06-02 08:51:34 +0200

    reformat pipe.py

--- pipe.py
+++ pipe.py
@@ -15,8 +15,8 @@ from piping import *
 
 tu = FreeCAD.Units.parseQuantity
 
-# This is the path to the dimensions table. 
-CSV_TABLE_PATH = os.path.join(OSEBasePiping.TABLE_PATH, 'pipe.csv' )
+# This is the path to the dimensions table.
+CSV_TABLE_PATH = os.path.join(OSEBasePiping.TABLE_PATH, 'pipe.csv')
 # It must contain unique values in the column "Name" and also, dimensions listened below.
 DIMENSIONS_USED = ["OD", "Thk"]
 
@@ -33,120 +33,131 @@ RELATIVE_EPSILON = 0.1
 
 
 class Pipe:
-	def __init__(self, document):
-		self.document = document
-		self.OD = tu("3 cm")
-		self.Thk = tu("0.5 cm")
-		self.H = tu("1 m")
+    def __init__(self, document):
+        self.document = document
+        self.OD = tu("3 cm")
+        self.Thk = tu("0.5 cm")
+        self.H = tu("1 m")
 
-	def checkDimensions(self):
-		if not (self.OD > tu("0 mm")):
-			raise UnplausibleDimensions("OD (inner diameter) of the pipe must be positive. It is %s instead"%(self.OD))
-		if not (2*self.Thk <= self.OD):
-			raise UnplausibleDimensions("2*Thk (2*Thickness) %s must be less than or equlat to OD (outer diameter)%s "%(2*self.Thk, self.OD))
-		if not (self.H > tu("0 mm")):
-			raise UnplausibleDimensions("Height H=%s must be positive"%self.H)
+    def checkDimensions(self):
+        if not (self.OD > tu("0 mm")):
+            raise UnplausibleDimensions(
+                "OD (inner diameter) of the pipe must be positive. It is %s instead" % (self.OD))
+        if not (2 * self.Thk <= self.OD):
+            raise UnplausibleDimensions(
+                "2*Thk (2*Thickness) %s must be less than or equlat to OD (outer diameter)%s " % (2 * self.Thk, self.OD))
+        if not (self.H > tu("0 mm")):
+            raise UnplausibleDimensions(
+                "Height H=%s must be positive" % self.H)
 
-	def create(self, convertToSolid):
-		""" A pipe which is a differences of two cilinders: outer cylinder - inner cylinder.
-		:param convertToSolid: if true, the resulting part will be solid.
-			if false, the resulting part will be a cut.
-		:return resulting part.
-		"""
-		self.checkDimensions()
-		# Create outer cylinder.
-		outer_cylinder = self.document.addObject("Part::Cylinder","OuterCylinder")
-		outer_cylinder.Radius = self.OD/2
-		outer_cylinder.Height = self.H
-		
-		# Create inner cylinder. It is a little bit longer than the outer cylider in both ends.
-		# This should prevent numerical problems when calculating difference
-		# between the outer and innter cylinder.
-		inner_cylinder = self.document.addObject("Part::Cylinder","InnerCylinder")
-		inner_cylinder.Radius = self.OD/2 - self.Thk
-		inner_cylinder.Height = self.H*(1+2*RELATIVE_EPSILON)
-		inner_cylinder.Placement.Base = FreeCAD.Vector(0,0,-self.H*RELATIVE_EPSILON)
-		pipe = self.document.addObject("Part::Cut","Pipe")
-		pipe.Base = outer_cylinder
-		pipe.Tool = inner_cylinder
+    def create(self, convertToSolid):
+        """ A pipe which is a differences of two cilinders: outer cylinder - inner cylinder.
+        :param convertToSolid: if true, the resulting part will be solid.
+                if false, the resulting part will be a cut.
+        :return resulting part.
+        """
+        self.checkDimensions()
+        # Create outer cylinder.
+        outer_cylinder = self.document.addObject(
+            "Part::Cylinder", "OuterCylinder")
+        outer_cylinder.Radius = self.OD / 2
+        outer_cylinder.Height = self.H
 
-		if convertToSolid:
-			# Before making a solid, recompute documents. Otherwise there will be
-			#    s = Part.Solid(Part.Shell(s))
-			#    <class 'Part.OCCError'>: Shape is null
-			# exception.
-			self.document.recompute()
-			# Now convert all parts to solid, and remove intermediate data.
-			solid = toSolid(self.document, pipe, "pipe (solid)")
-			# Remove previous (intermediate parts).
-			parts = nestedObjects(pipe)
-			# Document.removeObjects can remove multple objects, when we use
-			# parts directly. To prevent exceptions with deleted objects,
-			# use the name list instead.
-			names_to_remove = []
-			for part in parts:
-				if part.Name not in names_to_remove:
-					names_to_remove.append(part.Name)
-			for name in names_to_remove:
-				print("Deleting temporary objects %s."%name)
-				self.document.removeObject(name)
-			return solid
-		return pipe
+        # Create inner cylinder. It is a little bit longer than the outer cylider in both ends.
+        # This should prevent numerical problems when calculating difference
+        # between the outer and innter cylinder.
+        inner_cylinder = self.document.addObject(
+            "Part::Cylinder", "InnerCylinder")
+        inner_cylinder.Radius = self.OD / 2 - self.Thk
+        inner_cylinder.Height = self.H * (1 + 2 * RELATIVE_EPSILON)
+        inner_cylinder.Placement.Base = FreeCAD.Vector(
+            0, 0, -self.H * RELATIVE_EPSILON)
+        pipe = self.document.addObject("Part::Cut", "Pipe")
+        pipe.Base = outer_cylinder
+        pipe.Tool = inner_cylinder
+
+        if convertToSolid:
+            # Before making a solid, recompute documents. Otherwise there will be
+            #    s = Part.Solid(Part.Shell(s))
+            #    <class 'Part.OCCError'>: Shape is null
+            # exception.
+            self.document.recompute()
+            # Now convert all parts to solid, and remove intermediate data.
+            solid = toSolid(self.document, pipe, "pipe (solid)")
+            # Remove previous (intermediate parts).
+            parts = nestedObjects(pipe)
+            # Document.removeObjects can remove multple objects, when we use
+            # parts directly. To prevent exceptions with deleted objects,
+            # use the name list instead.
+            names_to_remove = []
+            for part in parts:
+                if part.Name not in names_to_remove:
+                    names_to_remove.append(part.Name)
+            for name in names_to_remove:
+                print("Deleting temporary objects %s." % name)
+                self.document.removeObject(name)
+            return solid
+        return pipe
 
 
 class PipeFromTable:
-	"""Create a part with dimensions from a CSV table."""
-	def __init__ (self, document, table):
-		self.document = document
-		self.table = table
-	
-	def create(self, partName, length, outputType):
-		row = self.table.findPart(partName)
-		if row is None:
-			print("Part not found")
-			return
-		if outputType == OUTPUT_TYPE_PARTS or outputType == OUTPUT_TYPE_SOLID:
-			pipe = Pipe(self.document)
-			pipe.OD = tu(row["OD"])
-			pipe.Thk = tu(row["Thk"])
-			pipe.H = length
-			part = pipe.create(outputType == OUTPUT_TYPE_SOLID)
-			#part.Label = partName
-			return part
-		elif outputType == OUTPUT_TYPE_FLAMINGO:
-			# See Code in pipeCmd.makePipe in the Flamingo workbench.
-			feature = self.document.addObject("Part::FeaturePython", "OSE-Pipe")
-			import pipeFeatures
-			DN = GetDnString(row)
-			OD = tu(row["OD"])
-			Thk = tu(row["Thk"])
-			part = pipeFeatures.Pipe(feature, DN=DN, OD=OD, thk=Thk, H=length)
-			feature.PRating = GetPressureRatingString(row)
-			feature.Profile = "" # Currently I do not know how to interprite table data as a profile.
-			PSize = ""
-			if "PSize" in row.keys():
-				feature.PSize = row["PSize"]
+    """Create a part with dimensions from a CSV table."""
 
-			feature.ViewObject.Proxy = 0
-			#feature.Label = partName
-    			return part
+    def __init__(self, document, table):
+        self.document = document
+        self.table = table
+
+    def create(self, partName, length, outputType):
+        row = self.table.findPart(partName)
+        if row is None:
+            print("Part not found")
+            return
+        if outputType == OUTPUT_TYPE_PARTS or outputType == OUTPUT_TYPE_SOLID:
+            pipe = Pipe(self.document)
+            pipe.OD = tu(row["OD"])
+            pipe.Thk = tu(row["Thk"])
+            pipe.H = length
+            part = pipe.create(outputType == OUTPUT_TYPE_SOLID)
+            #part.Label = partName
+            return part
+        elif outputType == OUTPUT_TYPE_FLAMINGO:
+            # See Code in pipeCmd.makePipe in the Flamingo workbench.
+            feature = self.document.addObject(
+                "Part::FeaturePython", "OSE-Pipe")
+            import pipeFeatures
+            DN = GetDnString(row)
+            OD = tu(row["OD"])
+            Thk = tu(row["Thk"])
+            part = pipeFeatures.Pipe(feature, DN=DN, OD=OD, thk=Thk, H=length)
+            feature.PRating = GetPressureRatingString(row)
+            # Currently I do not know how to interprite table data as a profile.
+            feature.Profile = ""
+            PSize = ""
+            if "PSize" in row.keys():
+                feature.PSize = row["PSize"]
+
+            feature.ViewObject.Proxy = 0
+            #feature.Label = partName
+            return part
 
 # Test macros.
+
+
 def TestPipe():
-	document = FreeCAD.activeDocument()
-	pipe = Pipe(document)
-	pipe.create(False)
-	document.recompute()
+    document = FreeCAD.activeDocument()
+    pipe = Pipe(document)
+    pipe.create(False)
+    document.recompute()
+
 
 def TestTable():
-	document = FreeCAD.activeDocument()
-	table = CsvTable(DIMENSIONS_USED)
-	table.load(CSV_TABLE_PATH)
-	pipe = PipeFromTable(document, table)
-	for i in range(0, len(table.data)):
-		print("Selecting row %d"%i)
-		partName = table.getPartName(i)
-		print("Creating part %s"%partName)
-		pipe.create(partName, tu("1m"), False)
-		document.recompute()
-
+    document = FreeCAD.activeDocument()
+    table = CsvTable(DIMENSIONS_USED)
+    table.load(CSV_TABLE_PATH)
+    pipe = PipeFromTable(document, table)
+    for i in range(0, len(table.data)):
+        print("Selecting row %d" % i)
+        partName = table.getPartName(i)
+        print("Creating part %s" % partName)
+        pipe.create(partName, tu("1m"), False)
+        document.recompute()
