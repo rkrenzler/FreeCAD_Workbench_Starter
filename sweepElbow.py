2353190a2a66f9815b04ce9a2728811b20c4478e
Ruslan <20125922+rkrenzler@users.noreply.github.com>
2018-06-09 14:18:37 +0200

    adjust sweep elbow to flamingo orientation
    Also clean up code.

--- sweepElbow.py
+++ sweepElbow.py
@@ -4,14 +4,12 @@
 # Create a sweep-elbow-fitting.
 
 import math
-import csv
 import os.path
 
 import FreeCAD
-import Part
 
 import OSEBasePiping
-from piping import *
+import piping
 
 parseQuantity = FreeCAD.Units.parseQuantity
 
@@ -19,6 +17,13 @@ parseQuantity = FreeCAD.Units.parseQuantity
 CSV_TABLE_PATH = os.path.join(OSEBasePiping.TABLE_PATH, "sweep-elbow.csv")
 # It must contain unique values in the column "Name" and also, dimensions listened below.
 DIMENSIONS_USED = ["BendAngle", "G", "J", "M", "POD", "PThk"]
+
+# The value RELATIVE_EPSILON is used to slightly change the size of parts
+# to prevent problems with boolean operations.
+# Keep this value very small.
+# For example, the outer bent part of the elbow dissaperas when it has
+# the same radius as the cylinder at the ends.
+RELATIVE_EPSILON = 0.000001
 
 
 class Dimensions:
@@ -42,13 +47,60 @@ class Dimensions:
         elif not (self.M > self.POD):
             errorMsg = "Socket outer diameter %s must be greater than pipe outer diameter =%s." % (
                 self.M, self.POD)
-        elif not (self.G > self.M / 2 + fitThk):
-            errorMsg = "Length G=%s must be larger than M/2 + fitting thickness (M-POD)/2 =%s." % (self.G,
+        elif not (self.J > self.M / 2 + fitThk):
+            errorMsg = "Length G=%s must be larger than M/2 + fitting thickness (M-POD)/2 =%s." % (self.J,
                                                                                                    self.M / 2 + fitThk)
-        elif not (self.H > self.G):
-            errorMsg = "Length H=%s must be larger than G=%s" % (
+        elif not (self.H > self.J):
+            errorMsg = "Length H=%s must be larger than J=%s" % (
                 self.H, self.G)
         return (len(errorMsg) == 0, errorMsg)
+
+    def calculateAuxiliararyPoints(self):
+        """Calculate auxiliarary points influenced by bentAngle, bentRadius (self.M/2)
+        and the distannce J.
+
+        See documentation picture sweep-elbow-cacluations.png. The code is similar to
+        Elbow.Dimensions.calculateAuxiliararyPoints.
+        """
+        rBend = self.M / 2.0
+        beta = 180 - float(self.BendAngle.getValueAs("deg"))
+        beta_rad = math.pi - float(self.BendAngle.getValueAs("rad"))
+
+        # Start with a normalized vector along th bisectrix of the x and y-axis.
+        # Then stretch and rotate this vector by -beta/2 and +beta/2 along the z axis.
+        bs = FreeCAD.Vector(1, 1, 0).normalize()
+        neg_rot = FreeCAD.Rotation(FreeCAD.Vector(0, 0, 1), -beta / 2)
+
+        p1 = neg_rot.multVec(bs * float(self.H))
+        p5 = neg_rot.multVec(bs * float(self.J))
+
+        pos_rot = FreeCAD.Rotation(FreeCAD.Vector(0, 0, 1), +beta / 2)
+        p6 = pos_rot.multVec(bs * float(self.J))
+
+        # p3 lies on the bissectrix.
+        p3 = bs * float(self.J / math.cos(beta_rad / 2.0))
+        # Calculate coordinates of the base cirle at the end of sweep.
+        a = self.calculateAuxiliararyLengths()
+        l = float(self.H) - float(a["a2"])
+        p2 = neg_rot.multVec(bs * l)
+        p4 = pos_rot.multVec(bs * l)
+
+        return {"p1": p1, "p2": p2, "p3": p3, "p4": p4, "p5": p5, "p6": p6}
+
+    def PID(self):
+        """Return the inner diamter of the pipe."""
+        return self.POD - 2 * self.PThk
+
+    def fitThk(self):
+        """Return thinckness of the fitting wall."""
+        return (self.M - self.POD) / 2.0
+
+    def calculateAuxiliararyLengths(self):
+
+        a1 = self.H - self.J
+        # Calculate socket Height.
+        a2 = self.H - self.J + self.fitThk()
+        return {"a1": a1, "a2": a2}
 
 
 class SweepElbow:
@@ -61,70 +113,101 @@ class SweepElbow:
         if not valid:
             raise UnplausibleDimensions(msg)
 
-    @staticmethod
-    def createBentCylinder(doc, group, r, l):
+    def createBentCylinder(self, group, rCirc, bendEps=0):
         """ Create 90° bent cylinder in x-z plane with radius r.
 
         :param group: Group where to add created objects.
-        :param r: Radius of the cylinder.
-        :param l: Distance to the origin (0,0,0).
+        :param rCirc: Radius of the cylinder.
+        :param bendEps: Make some dimensions smaller by (1+endEps) factor. A positive bendEps
+            is used when creating outer part, to prevent some artifacts, when substracting
+            the inner part from the outher part.
+            This parameter is not used now.
 
         See documentation picture sweep-elbow-cacluations.png
         """
-        p1 = FreeCAD.Vector(0, 0, -l)
-        # Add cylinder
-        base = doc.addObject("Part::Circle", "Base")
-        base.Radius = r
-        base.Placement.Base = p1
+        # Convert alpha to degree value
+        aux = self.dims.calculateAuxiliararyPoints()
+
+        alpha = float(self.dims.BendAngle.getValueAs("deg"))
+        rBend = (aux["p3"] - aux["p5"]).Length
+
+        # Calculate coordinates of the base circle.
+        # Add cylinder.
+        base = self.document.addObject("Part::Circle", "Base")
+        base.Radius = rCirc
+        base.Placement.Base = aux["p5"]
+        base.Placement.Rotation = FreeCAD.Rotation(
+            FreeCAD.Vector(0, 0, 1), aux["p5"])
+
         # Add trajectory
-        p3 = FreeCAD.Vector(l, 0, -l)
-        trajectory = doc.addObject("Part::Circle", "Trajectory")
-        trajectory.Radius = l
-        trajectory.Angle0 = 90
-        trajectory.Angle1 = 180
-        trajectory.Placement = FreeCAD.Placement(p3, FreeCAD.Rotation(
-            FreeCAD.Vector(1, 0, 0), 90), FreeCAD.Vector(0, 0, 0))
+        trajectory = self.document.addObject("Part::Circle", "Trajectory")
+        trajectory.Radius = rBend
+        trajectory.Angle0 = 225 - alpha / 2
+        trajectory.Angle1 = 225 + alpha / 2
+        trajectory.Placement.Base = aux["p3"]
+
         # Sweep the circle along the trajectory.
-        sweep = doc.addObject('Part::Sweep', 'Sweep')
+        sweep = self.document.addObject('Part::Sweep', 'Sweep')
         sweep.Sections = [base]
         sweep.Spine = trajectory
         sweep.Solid = True
-        group.addObjects([trajectory, sweep])
+        group.addObjects([trajectory, base, sweep])
         return sweep
 
     def createOuterPart(self, group):
-        """Create bending part and socket cylinders.
-
-        See documentation picture sweep-elbow-cacluations.png.
-        """
-        # Create a bent part.
-        bentPart = SweepElbow.createBentCylinder(
-            self.document, group, self.dims.POD / 2.0, self.dims.G)
-        # Create vertical socket (on the bottom).
-        socket1 = self.document.addObject("Part::Cylinder", "Socket1")
-        # Calculate wall thickness of the fitting.
-        fitThk = (self.dims.M - self.dims.POD) / 2.0
-        socket1.Radius = self.dims.M / 2.0
-        # Calculate socket Height.
-        a2 = self.dims.H - self.dims.G + fitThk
-        socket1.Height = a2
-        # Calculate socket position.
-        p2 = FreeCAD.Vector(0, 0, -self.dims.H)
+        aux = self.dims.calculateAuxiliararyPoints()
+        p1 = aux["p1"]
+        p2 = aux["p2"]
+        p4 = aux["p4"]
+        # Make the outer part slightly larger. Otherwise it can be shown incorrectly after
+        # the substraction of the inner part.
+        bentPart = self.createBentCylinder(
+            group, ((self.dims.PID() / 2 + self.dims.fitThk()) * (1 + RELATIVE_EPSILON)), RELATIVE_EPSILON)
+        # Create socket along the z axis.
+        socket1 = self.document.addObject("Part::Cylinder", "OuterSocket1")
+        socket1.Radius = self.dims.M / 2
+        socket1.Height = float(self.dims.H) - p2.Length
         socket1.Placement.Base = p2
-        # Calculate second socket (on the right).
-        socket2 = self.document.addObject("Part::Cylinder", "Socket2")
-        socket2.Radius = self.dims.M / 2.0
-        socket2.Height = a2
-        p3 = FreeCAD.Vector(self.dims.H - a2, 0, 0)
-        # Rotate the socket and bring it to the right end of the fitting.
-        socket2.Placement = FreeCAD.Placement(p3, FreeCAD.Rotation(
-            FreeCAD.Vector(0, 1, 0), 90), FreeCAD.Vector(0, 0, 0))
+        socket1.Placement.Rotation = FreeCAD.Rotation(
+            FreeCAD.Vector(0, 0, 1), p2)
+#        socket1.Placement = FreeCAD.Placement(p2, FreeCAD.Rotation(FreeCAD.Vector(0, 0, 1), p2),
+#                                              FreeCAD.Vector(0, 0, 0))
+        # Create socket along the bent part.
+        socket2 = self.document.addObject("Part::Cylinder", "OuterSocket2")
+        socket2.Radius = socket1.Radius
+        socket2.Height = socket1.Height
+        socket2.Placement = FreeCAD.Placement(p4, FreeCAD.Rotation(FreeCAD.Vector(0, 0, 1), p4),
+                                              FreeCAD.Vector(0, 0, 0))
         outer = self.document.addObject("Part::MultiFuse", "Outer")
         outer.Shapes = [bentPart, socket1, socket2]
         group.addObject(outer)
         return outer
 
     def createInnerPart(self, group):
+        aux = self.dims.calculateAuxiliararyPoints()
+        pid = self.dims.POD - self.dims.PThk * 2
+        bentPart = self.createBentCylinder(group, pid / 2)
+        # Add sockets
+        socket1 = self.document.addObject("Part::Cylinder", "Socket1")
+        socket1.Radius = self.dims.POD / 2
+        socket1.Height = self.dims.H
+        socket1.Placement.Base = aux["p5"]
+        socket1.Placement.Rotation = FreeCAD.Rotation(
+            FreeCAD.Vector(0, 0, 1), aux["p5"])
+
+        socket2 = self.document.addObject("Part::Cylinder", "Socket2")
+        socket2.Radius = socket1.Radius
+        socket2.Height = socket1.Height
+        socket2.Placement.Base = aux["p6"]
+        socket2.Placement.Rotation = FreeCAD.Rotation(
+            FreeCAD.Vector(0, 0, 1), aux["p6"])
+
+        inner = self.document.addObject("Part::MultiFuse", "Inner")
+        inner.Shapes = [bentPart, socket1, socket2]
+        group.addObject(inner)
+        return inner
+
+    def createInnerPartOld(self, group):
         """Create inner bending part and socket cylinders.
         See documentation picture sweep-elbow-cacluations.png.
 
@@ -181,9 +264,9 @@ class SweepElbow:
             # exception.
             self.document.recompute()
             # Now convert all parts to solid, and remove intermediate data.
-            solid = toSolid(self.document, elbow, "sweep elbow (solid)")
+            solid = piping.toSolid(self.document, elbow, "sweep elbow (solid)")
             # Remove previous (intermediate parts).
-            parts = nestedObjects(group)
+            parts = piping.nestedObjects(group)
             # Document.removeObjects can remove multple objects, when we use
             # parts directly. To prevent exceptions with deleted objects,
             # use the name list instead.
@@ -209,7 +292,7 @@ class SweepElbowFromTable:
     def getPThk(cls, row):
         """ For compatibility results, if there is no "PThk" dimension, calculate it
         from "PID" and "POD" """
-        if not "PThk" in row.keys():
+        if "PThk" not in row.keys():
             return (parseQuantity(row["POD"]) - parseQuantity(row["PID"])) / 2.0
         else:
             return parseQuantity(row["PThk"])
@@ -233,10 +316,10 @@ class SweepElbowFromTable:
         dims.POD = parseQuantity(row["POD"])
         dims.Thk = SweepElbowFromTable.getPThk(row)
 
-        if outputType == OUTPUT_TYPE_PARTS or outputType == OUTPUT_TYPE_SOLID:
+        if outputType == piping.OUTPUT_TYPE_PARTS or outputType == piping.OUTPUT_TYPE_SOLID:
             elbow = SweepElbow(self.document)
             elbow.dims = dims
-            part = elbow.create(outputType == OUTPUT_TYPE_SOLID)
+            part = elbow.create(outputType == piping.OUTPUT_TYPE_SOLID)
             part.Label = "OSE-SweepElbow"
             return part
 
@@ -248,7 +331,7 @@ class SweepElbowFromTable:
             builder = flSweepElbow.SweepElbowBuilder(self.document)
             builder.dims = dims
             part = builder.create(feature)
-            feature.PRating = GetPressureRatingString(row)
+            feature.PRating = piping.GetPressureRatingString(row)
             feature.PSize = self.getPSize(row)
             feature.ViewObject.Proxy = 0
             feature.PartNumber = partNumber
@@ -265,15 +348,15 @@ def TestSweepElbow():
 
 def TestSweepElbowTable():
     document = FreeCAD.activeDocument()
-    table = CsvTable(DIMENSIONS_USED)
+    table = piping.CsvTable(DIMENSIONS_USED)
     table.load(CSV_TABLE_PATH)
     builder = SweepElbowFromTable(document, table)
     for i in range(0, len(table.data)):
         print("Selecting row %d" % i)
         partNumber = table.getPartKey(i)
         print("Creating part %s" % partNumber)
-        builder.create(partNumber, OUTPUT_TYPE_FLAMINGO)
+        builder.create(partNumber, piping.OUTPUT_TYPE_FLAMINGO)
         document.recompute()
 
-# TestSweepElbow()
+#TestSweepElbow()
 # TestSweepElbowTable()
