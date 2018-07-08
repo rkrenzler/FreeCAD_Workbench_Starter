a9e85f75efbc3daa057bc159d90288ce08cdecb2
Ruslan <20125922+rkrenzler@users.noreply.github.com>
2018-07-08 10:24:19 +0200

    reformat ElbowGui.py

--- OsePiping/ElbowGui.py
+++ OsePiping/ElbowGui.py
@@ -9,21 +9,22 @@ import CreatePartGui
 
 
 class MainDialog(CreatePartGui.BaseDialog):
-	def __init__(self, document, table):
-		params = CreatePartGui.DialogParams()
-		params.document = document
-		params.table = table
-		params.dialogTitle = "Create Elbow"
-		params.fittingType = "Elbow"
-		params.dimensionsPixmap = "elbow-dimensions.png"
-		params.explanationText = "<html><head/><body><p>To construct an elbow only these dimensions are used: BendingAngle, H, J, M, POD, and PThk. In Additinon, Flamingo uses the Schedule dimension if it is present in the table. All other dimensions are used for inromation only. </p></body></html>"
-		params.settingsName = "elbow user input"
-		params.keyColumnName = "PartNumber"
-		super(MainDialog, self).__init__(params)
+    def __init__(self, document, table):
+        params = CreatePartGui.DialogParams()
+        params.document = document
+        params.table = table
+        params.dialogTitle = "Create Elbow"
+        params.fittingType = "Elbow"
+        params.dimensionsPixmap = "elbow-dimensions.png"
+        params.explanationText = "<html><head/><body><p>To construct an elbow only these dimensions are used: BendingAngle, H, J, M, POD, and PThk. In Additinon, Flamingo uses the Schedule dimension if it is present in the table. All other dimensions are used for inromation only. </p></body></html>"
+        params.settingsName = "elbow user input"
+        params.keyColumnName = "PartNumber"
+        super(MainDialog, self).__init__(params)
 
-	def createNewPart(self, document, table, partName, outputType):
-			builder = Elbow.ElbowFromTable(self.params.document, self.params.table)
-			return builder.create(partName, outputType)
+    def createNewPart(self, document, table, partName, outputType):
+        builder = Elbow.ElbowFromTable(self.params.document, self.params.table)
+        return builder.create(partName, outputType)
+
 
 def GuiCheckTable():
-	return CreatePartGui.GuiCheckTable(Elbow.CSV_TABLE_PATH, Elbow.DIMENSIONS_USED)
+    return CreatePartGui.GuiCheckTable(Elbow.CSV_TABLE_PATH, Elbow.DIMENSIONS_USED)
