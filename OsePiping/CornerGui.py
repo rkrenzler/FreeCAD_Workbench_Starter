970320594cb503366cd37320d2c303fc2222e2be
Ruslan <20125922+rkrenzler@users.noreply.github.com>
2018-07-09 22:03:09 +0200

    reformat CornerGui

--- OsePiping/CornerGui.py
+++ OsePiping/CornerGui.py
@@ -6,23 +6,26 @@
 import Corner
 import CreatePartGui
 
-class MainDialog(CreatePartGui.BaseDialog):
-	def __init__(self, document, table):
-		params = CreatePartGui.DialogParams()
-		params.document = document
-		params.table = table
-		params.dialogTitle = "Create corner"
-		params.selectionDialogTitle = "Select corner"
-		params.fittingType = "Corner"
-		params.dimensionsPixmap = "corner-dimensions.png"
-		params.explanationText = "<html><head/><body><p>To construct a part, only these dimensions are used: G, H, M, POD and PThk. All other dimensions are used for inromation.</p></body></html>"
-		params.settingsName = "corner user input"
-		params.keyColumnName = "PartNumber"
-		super(MainDialog, self).__init__(params)
 
-	def createNewPart(self, document, table, partName, outputType):
-			builder = Corner.CornerFromTable(self.params.document, self.params.table)
-			return builder.create(partName, outputType)
+class MainDialog(CreatePartGui.BaseDialog):
+    def __init__(self, document, table):
+        params = CreatePartGui.DialogParams()
+        params.document = document
+        params.table = table
+        params.dialogTitle = "Create corner"
+        params.selectionDialogTitle = "Select corner"
+        params.fittingType = "Corner"
+        params.dimensionsPixmap = "corner-dimensions.png"
+        params.explanationText = "<html><head/><body><p>To construct a part, only these dimensions are used: G, H, M, POD and PThk. All other dimensions are used for inromation.</p></body></html>"
+        params.settingsName = "corner user input"
+        params.keyColumnName = "PartNumber"
+        super(MainDialog, self).__init__(params)
+
+    def createNewPart(self, document, table, partName, outputType):
+        builder = Corner.CornerFromTable(
+            self.params.document, self.params.table)
+        return builder.create(partName, outputType)
+
 
 def GuiCheckTable():
-	return CreatePartGui.GuiCheckTable(Corner.CSV_TABLE_PATH, Corner.DIMENSIONS_USED)
+    return CreatePartGui.GuiCheckTable(Corner.CSV_TABLE_PATH, Corner.DIMENSIONS_USED)
