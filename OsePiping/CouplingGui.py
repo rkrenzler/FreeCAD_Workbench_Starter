77a723a36d162fd15b61e745a2386dbb70fd462b
Ruslan <20125922+rkrenzler@users.noreply.github.com>
2018-07-09 22:05:02 +0200

    reformat CouplingGui

--- OsePiping/CouplingGui.py
+++ OsePiping/CouplingGui.py
@@ -8,22 +8,23 @@ import CreatePartGui
 
 
 class MainDialog(CreatePartGui.BaseDialog):
-	def __init__(self, document, table):
-		params = CreatePartGui.DialogParams()
-		params.document = document
-		params.table = table
-		params.dialogTitle = "Create Coupling"
-		params.fittingType = "Coupling"
-		params.dimensionsPixmap = "coupling-dimensions.png"
-		params.explanationText = "<html><head/><body><p>To construct a coupling we use these dimensions, elbow only these dimensions are used: alpha, L, N,  M, M1, POD, POD1, PThk, and PThk1. In Additinon, Flamingo uses the Schedule dimension if it is present in the table. All other dimensions are used for inromation only. </p></body></html>"
-		params.settingsName = "coupling user input"
-		params.keyColumnName = "PartNumber"
-		super(MainDialog, self).__init__(params)
+    def __init__(self, document, table):
+        params = CreatePartGui.DialogParams()
+        params.document = document
+        params.table = table
+        params.dialogTitle = "Create Coupling"
+        params.fittingType = "Coupling"
+        params.dimensionsPixmap = "coupling-dimensions.png"
+        params.explanationText = "<html><head/><body><p>To construct a coupling we use these dimensions, elbow only these dimensions are used: alpha, L, N,  M, M1, POD, POD1, PThk, and PThk1. In Additinon, Flamingo uses the Schedule dimension if it is present in the table. All other dimensions are used for inromation only. </p></body></html>"
+        params.settingsName = "coupling user input"
+        params.keyColumnName = "PartNumber"
+        super(MainDialog, self).__init__(params)
 
-	def createNewPart(self, document, table, partName, outputType):
-			builder = Coupling.CouplingFromTable(self.params.document, self.params.table)
-			return builder.create(partName, outputType)
+    def createNewPart(self, document, table, partName, outputType):
+        builder = Coupling.CouplingFromTable(
+            self.params.document, self.params.table)
+        return builder.create(partName, outputType)
 
 
 def GuiCheckTable():
-	return CreatePartGui.GuiCheckTable(Coupling.CSV_TABLE_PATH, Coupling.DIMENSIONS_USED)
+    return CreatePartGui.GuiCheckTable(Coupling.CSV_TABLE_PATH, Coupling.DIMENSIONS_USED)
