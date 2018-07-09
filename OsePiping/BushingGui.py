cd6a47e5a9b2e381152b498bf5c88bdb47098dd5
Ruslan <20125922+rkrenzler@users.noreply.github.com>
2018-07-09 22:02:18 +0200

    reformat bushing

--- OsePiping/BushingGui.py
+++ OsePiping/BushingGui.py
@@ -8,21 +8,22 @@ import CreatePartGui
 
 
 class MainDialog(CreatePartGui.BaseDialog):
-	def __init__(self, document, table):
-		params = CreatePartGui.DialogParams()
-		params.document = document
-		params.table = table
-		params.dialogTitle = "Create Bushing"
-		params.fittingType = "Bushing"
-		params.dimensionsPixmap = "bushing-dimensions.png"
-		params.explanationText = "<html><head/><body><p>To construct a part, only these dimensions are used: L, N, POD, POD1 and PThk1. All other dimensions are used for inromation.</p></body></html>"
-		params.settingsName = "bushing user input"
-		params.keyColumnName = "PartNumber"
-		super(MainDialog, self).__init__(params)
+    def __init__(self, document, table):
+        params = CreatePartGui.DialogParams()
+        params.document = document
+        params.table = table
+        params.dialogTitle = "Create Bushing"
+        params.fittingType = "Bushing"
+        params.dimensionsPixmap = "bushing-dimensions.png"
+        params.explanationText = "<html><head/><body><p>To construct a part, only these dimensions are used: L, N, POD, POD1 and PThk1. All other dimensions are used for inromation.</p></body></html>"
+        params.settingsName = "bushing user input"
+        params.keyColumnName = "PartNumber"
+        super(MainDialog, self).__init__(params)
 
-	def createNewPart(self, document, table, partName, outputType):
-			builder = Bushing.BushingFromTable(self.params.document, self.params.table)
-			return builder.create(partName, outputType)
+    def createNewPart(self, document, table, partName, outputType):
+        builder = Bushing.BushingFromTable(self.params.document, self.params.table)
+        return builder.create(partName, outputType)
+
 
 def GuiCheckTable():
-	return CreatePartGui.GuiCheckTable(Bushing.CSV_TABLE_PATH, Bushing.DIMENSIONS_USED)
+    return CreatePartGui.GuiCheckTable(Bushing.CSV_TABLE_PATH, Bushing.DIMENSIONS_USED)
