62afa13d16e6cd2d438585d18681248de7917744
Ruslan <20125922+rkrenzler@users.noreply.github.com>
2018-07-09 22:06:41 +0200

    reformat cross

--- OsePiping/CrossGui.py
+++ OsePiping/CrossGui.py
@@ -9,22 +9,24 @@ import os.path
 import Cross
 import CreatePartGui
 
-class MainDialog(CreatePartGui.BaseDialog):
-	def __init__(self, document, table):
-		params = CreatePartGui.DialogParams()
-		params.document = document
-		params.table = table
-		params.dialogTitle = "Create Cross"
-		params.fittingType = "Cross"
-		params.dimensionsPixmap = "cross-dimensions.png"
-		params.explanationText = "<html><head/><body><p>To construct an cross only these dimensions are used: G, G1, H, H1, L, L1, M, M1, POD, POD1, PThk, Pthk1. In Additinon, Flamingo uses the Schedule dimension if it is present in the table. All other dimensions are used for inromation only. </p></body></html>"
-		params.settingsName = "cross user input"
-		params.keyColumnName = "PartNumber"
-		super(MainDialog, self).__init__(params)
 
-	def createNewPart(self, document, table, partName, outputType):
-			builder = Cross.CrossFromTable(self.params.document, self.params.table)
-			return builder.create(partName, outputType)
+class MainDialog(CreatePartGui.BaseDialog):
+    def __init__(self, document, table):
+        params = CreatePartGui.DialogParams()
+        params.document = document
+        params.table = table
+        params.dialogTitle = "Create Cross"
+        params.fittingType = "Cross"
+        params.dimensionsPixmap = "cross-dimensions.png"
+        params.explanationText = "<html><head/><body><p>To construct an cross only these dimensions are used: G, G1, H, H1, L, L1, M, M1, POD, POD1, PThk, Pthk1. In Additinon, Flamingo uses the Schedule dimension if it is present in the table. All other dimensions are used for inromation only. </p></body></html>"
+        params.settingsName = "cross user input"
+        params.keyColumnName = "PartNumber"
+        super(MainDialog, self).__init__(params)
+
+    def createNewPart(self, document, table, partName, outputType):
+        builder = Cross.CrossFromTable(self.params.document, self.params.table)
+        return builder.create(partName, outputType)
+
 
 def GuiCheckTable():
-	return CreatePartGui.GuiCheckTable(Cross.CSV_TABLE_PATH, Cross.DIMENSIONS_USED)
+    return CreatePartGui.GuiCheckTable(Cross.CSV_TABLE_PATH, Cross.DIMENSIONS_USED)
