663ff04182e3d5d35dd40f4a79b1abb64c2a6b3a
Ruslan <20125922+rkrenzler@users.noreply.github.com>
2018-07-09 22:16:34 +0200

    reformat SweepElbowGui

--- OsePiping/SweepElbowGui.py
+++ OsePiping/SweepElbowGui.py
@@ -8,21 +8,23 @@ import CreatePartGui
 
 
 class MainDialog(CreatePartGui.BaseDialog):
-	def __init__(self, document, table):
-		params = CreatePartGui.DialogParams()
-		params.document = document
-		params.table = table
-		params.dialogTitle = "Create Sweep Elbow"
-		params.fittingType = "SweepElbow"
-		params.dimensionsPixmap = "sweep-elbow-dimensions.png"
-		params.explanationText = "<html><head/><body><p>Only dimensions used are: BendAngle, H, J, M POD, PThk. All other dimensions are used for inromation.</p></body></html>"
-		params.settingsName = "sweep elbow user input"
-		params.keyColumnName = "PartNumber"
-		super(MainDialog, self).__init__(params)
+    def __init__(self, document, table):
+        params = CreatePartGui.DialogParams()
+        params.document = document
+        params.table = table
+        params.dialogTitle = "Create Sweep Elbow"
+        params.fittingType = "SweepElbow"
+        params.dimensionsPixmap = "sweep-elbow-dimensions.png"
+        params.explanationText = "<html><head/><body><p>Only dimensions used are: BendAngle, H, J, M POD, PThk. All other dimensions are used for inromation.</p></body></html>"
+        params.settingsName = "sweep elbow user input"
+        params.keyColumnName = "PartNumber"
+        super(MainDialog, self).__init__(params)
 
-	def createNewPart(self, document, table, partName, outputType):
-			builder = SweepElbow.SweepElbowFromTable(self.params.document, self.params.table)
-			return builder.create(partName, outputType)
+    def createNewPart(self, document, table, partName, outputType):
+        builder = SweepElbow.SweepElbowFromTable(
+            self.params.document, self.params.table)
+        return builder.create(partName, outputType)
+
 
 def GuiCheckTable():
-	return CreatePartGui.GuiCheckTable(SweepElbow.CSV_TABLE_PATH, SweepElbow.DIMENSIONS_USED)
+    return CreatePartGui.GuiCheckTable(SweepElbow.CSV_TABLE_PATH, SweepElbow.DIMENSIONS_USED)
