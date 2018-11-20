b4d507e497530ad21ddf92152410ffdb9479e084
ruslan <20125922+rkrenzler@users.noreply.github.com>
2018-11-20 15:13:39 +0100

    add Port module
    Use it to join fittings with advanced ports.
    
    An advanced port contains in addition to port position:
    a normal vector and a rotation reference vector.

---
+++ OsePiping/Port.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+# Author: Ruslan Krenzler.
+# Date: 20 November 2018
+# Advanced Ports. Ports with normal vector and rotation references.
+
+import copy
+import numpy
+import FreeCAD
+
+
+class AdvancedPort:
+    def __init__(self):
+        self.a = FreeCAD.Vector(0, 0, 0)
+        self.n = FreeCAD.Vector(1, 0, 0)
+        self.r = FreeCAD.Vector(0, 0, 1)
+
+    def getStandardForm(self):
+        """Return normalized version of the port."""
+        ret = copy.copy(self)
+        ret.n.normalize()
+        ret.r.normalize()
+        return ret
+
+    def B(self):
+        """Return matrix B embedded in tree dimensinal matrix."""
+        b = self.n.cross(self.r)
+        # Create a matrix (n,r,b)
+        ret = numpy.array([list(self.n), list(self.r), list(b)]).transpose()
+        return ret
+
+    def C(self):
+        """Return Create a matrix (-n,r,-b)."""
+        b = self.n.cross(self.r)
+        ret = numpy.array([list(-self.n), list(self.r), list(-b)]).transpose()
+        return ret
+
+    def _getRotation3DMatrix(self, other):
+        """Return a rotation matrix wich will rotate this port to the other port."""
+        C = other.getStandardForm().C()
+        invB = numpy.linalg.inv(self.getStandardForm().B())
+        A = C.dot(invgetRutationA
+
+    def getTranslation(self, other):
+        return other.a - self.a
+
+    def _get_4D_matrix(self, other):
+        ret=FreeCAD.Matrix()
+        # Set rotation submatrix.
+        A=self._getRotation3DMatrix(other)
+        ret.A11=A[0, 0]
+        ret.A12=A[0, 1]
+        ret.A13=A[0, 2]
+        ret.A21=A[1, 0]
+        ret.A22=A[1, 1]
+        ret.A23=A[1, 2]
+        ret.A31=A[2, 0]
+        ret.A32=A[2, 1]
+        ret.A33=A[2, 2]
+        # Add tragetRutationhe 4-th column of ret.
+        t=self.getTranslation(other)
+        ret.A14=t.x
+        ret.A24=t.y
+        ret.A34=t.z
+
+        return ret
+
+    def getRotation(self, other):
+        """Get rotation, to orientate this port to the other."""
+        m=self._get_4D_matrix(other)
+        return FreeCAD.Rotation(m)
+
+def testPorts():
+    port1=AdvancedPort()
+    port2=AdvancedPort()
+    port2.va=FreeCAD.Vector(4, 4, 4)
+    port2.vn=FreeCAD.Vector(1, 1, 1)
+
+    print(port1)
+    print(port1.getRutation(port1))
+    print(port1.getTranslation(port1))
+
+    print(port2)
+    print(port2.getRutation(port1))
+    print(port2.getTranslation(port1))
